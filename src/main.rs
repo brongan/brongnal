2a0109f90f374b8507beee897f2b2799c71e45f9
Brennan Tracy <brennantracy4@gmail.com>
2024-04-01 10:52:17 -0700

    More progress.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use anyhow::{anyhow, Context, Result};
-use sha2::Sha256;
-use blake2::{Blake2b, Blake2b512, Digest};
+use blake2::{Blake2b512, Digest};
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
     ChaCha20Poly1305, Nonce,
 };
-use ed25519_dalek::{SecretKey, Signature, Signer, SigningKey, Verifier, VerifyingKey};
+use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
+use hkdf::Hkdf;
+use sha2::Sha256;
 use std::sync::{mpsc, Arc};
 use std::thread;
-use x25519_dalek::{EphemeralSecret, PublicKey, ReusableSecret, StaticSecret};
-use hkdf::Hkdf;
-use hex_literal::hex;
+use x25519_dalek::{
+    PublicKey as X25519PublicKey, ReusableSecret as X25519ReusableSecret,
+    StaticSecret as X25519StaticSecret,
+};
 
 const NTHREADS: i32 = 16;
 const NONCE_LEN: usize = 12;
@@ -82,7 +84,10 @@ fn threads_test() -> Result<()> {
     Ok(())
 }
 
-fn sign_bundle(signing_key: &SigningKey, key_pairs: &[(StaticSecret, PublicKey)]) -> Signature {
+fn sign_bundle(
+    signing_key: &SigningKey,
+    key_pairs: &[(X25519StaticSecret, X25519PublicKey)],
+) -> Signature {
     let mut hasher = Blake2b512::new();
     hasher.update(key_pairs.len().to_be_bytes());
     for key_pair in key_pairs {
@@ -93,7 +98,7 @@ fn sign_bundle(signing_key: &SigningKey, key_pairs: &[(StaticSecret, PublicKey)]
 
 fn verify_bundle(
     verifying_key: &VerifyingKey,
-    public_keys: &[PublicKey],
+    public_keys: &[X25519PublicKey],
     signature: &Signature,
 ) -> Result<(), ed25519_dalek::ed25519::Error> {
     let mut hasher = Blake2b512::new();
@@ -106,14 +111,14 @@ fn verify_bundle(
 
 struct X3DHPreKey {
     signature: Signature,
-    bundle: Vec<(StaticSecret, PublicKey)>,
+    bundle: Vec<(X25519StaticSecret, X25519PublicKey)>,
 }
 
 fn x3dh_pre_key(signing_key: &SigningKey, num_keys: u32) -> X3DHPreKey {
     let bundle: Vec<_> = (0..num_keys)
         .map(|_| {
-            let pkey = StaticSecret::random();
-            let pubkey = PublicKey::from(&pkey);
+            let pkey = X25519StaticSecret::random();
+            let pubkey = X25519PublicKey::from(&pkey);
             (pkey, pubkey)
         })
         .collect();
@@ -122,55 +127,72 @@ fn x3dh_pre_key(signing_key: &SigningKey, num_keys: u32) -> X3DHPreKey {
 }
 
 struct SignedPreKey {
+    pre_key: X25519PublicKey,
     signature: Signature,
-    pre_key: PublicKey,
 }
 
 struct X3DHInitialResponse {
     identity_key: VerifyingKey,
     signed_pre_key: SignedPreKey,
-    one_time_key: Option<PublicKey>,
+    one_time_key: Option<X25519PublicKey>,
 }
 
 struct X3DHInitiateResult {
     identity_key: VerifyingKey,
-    ephemeral_key: EphemeralSecret,
-    //secret_key:  
+    ephemeral_key: X25519PublicKey,
+    secret_key: [u8; 32],
+    one_time_key: Option<X25519PublicKey>,
+}
+
+fn kdf(prk: &[u8]) -> Result<[u8; 32]> {
+    // TODO double check output of DH is cryptographically strong psuedorandom.
+    let hk = Hkdf::<Sha256>::from_prk(&prk).map_err(|e| anyhow!("Failed to generate hkdf: {e}"))?;
+    let mut okm = [0u8; 32];
+    hk.expand(b"sender key", &mut okm)
+        .map_err(|e| anyhow!("Failed to expand hkdf: {e}"))?;
+    Ok(okm)
 }
 
 fn x3dh_initiate_send_get_sk(
-    x3dh_initial_response: &X3DHInitialResponse,
-    sender_key: &SecretKey,
-) -> Result<()> {
+    identity_key: VerifyingKey,
+    signed_pre_key: SignedPreKey,
+    one_time_key: Option<X25519PublicKey>,
+    sender_key: SigningKey,
+) -> Result<X3DHInitiateResult> {
     let _ = verify_bundle(
-        &x3dh_initial_response.identity_key,
-        &[x3dh_initial_response.signed_pre_key.pre_key],
-        &x3dh_initial_response.signed_pre_key.signature,
+        &identity_key,
+        &[signed_pre_key.pre_key],
+        &signed_pre_key.signature,
     )
     .map_err(|e| anyhow!("Failed to verify bundle: {e}"));
 
-    let reusable_secret = ReusableSecret::random();
-    let sender_key = StaticSecret::from(*sender_key);
-    // const DH1 = await sodium.crypto_scalarmult(senderX, signedPreKey);
-    // const DH2 = await sodium.crypto_scalarmult(ephSecret, recipientX);
-    // const DH3 = await sodium.crypto_scalarmult(ephSecret, signedPreKey);
-    let dh1 = sender_key.diffie_hellman(&x3dh_initial_response.signed_pre_key.pre_key);
-    let dh2 = reusable_secret.diffie_hellman(&PublicKey::from(x3dh_initial_response.identity_key.to_montgomery().to_bytes()));
-    let dh3 = reusable_secret.diffie_hellman(&x3dh_initial_response.signed_pre_key.pre_key);
+    let reusable_secret = X25519ReusableSecret::random();
+    let dh1 = X25519StaticSecret::from(sender_key.to_scalar_bytes())
+        .diffie_hellman(&signed_pre_key.pre_key);
+    let dh2 = reusable_secret.diffie_hellman(&X25519PublicKey::from(
+        identity_key.to_montgomery().to_bytes(),
+    ));
+    let dh3 = reusable_secret.diffie_hellman(&signed_pre_key.pre_key);
 
-    // let sk = if let Some(one_time_key) = x3dh_initial_response.one_time_key {
-    //     let dh4 = ephemeral.diffie_hellman(&one_time_key);
-    //     kdf(dh1, dh2, dh3, dh4)
-    // } else {
-    let prk: Vec<u8> = [dh1.to_bytes(), dh2.to_bytes(), dh3.to_bytes()].concat();
-    let hk = Hkdf::<Sha256>::from_prk(&prk).expect("PRK should be large enough");
-    let mut okm = [0u8; 42];
-    let info = hex!("sender key");
-    hk.expand(&info, &mut okm).expect("42 is a valid length for Sha256 to output");
-    //};
+    let secret_key = if let Some(one_time_key) = one_time_key {
+        let dh4 = reusable_secret.diffie_hellman(&one_time_key);
+        kdf(&[
+            dh1.to_bytes(),
+            dh2.to_bytes(),
+            dh3.to_bytes(),
+            dh4.to_bytes(),
+        ]
+        .concat())
+    } else {
+        kdf(&[dh1.to_bytes(), dh2.to_bytes(), dh3.to_bytes()].concat())
+    }?;
 
-
-    Ok(())
+    Ok(X3DHInitiateResult {
+        identity_key,
+        ephemeral_key: X25519PublicKey::from(&reusable_secret),
+        secret_key,
+        one_time_key,
+    })
 }
 
 fn main() {
