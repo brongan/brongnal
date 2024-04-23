2b43129990c9f0c51d8570fdbc09bb749f6ea858
Brennan Tracy <brennantracy4@gmail.com>
2024-04-22 19:04:31 -0700

    Add GRPC reflection.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,23 @@
-use server::service::brongnal_server::BrongnalServer;
+use server::proto::{brongnal_server::BrongnalServer, FILE_DESCRIPTOR_SET};
 use server::MemoryServer;
-use std::net::{IpAddr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr};
 use tonic::transport::Server;
+use tonic_reflection::server::Builder;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let server_addr = (IpAddr::V6(Ipv6Addr::UNSPECIFIED), 8080);
-    let service = MemoryServer::default();
+    let reflection_service = Builder::configure()
+        .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+    let server_addr = (IpAddr::V4(Ipv4Addr::UNSPECIFIED), 8080).into();
+
+    println!("Brongnal Server listening at: {server_addr}");
 
     Server::builder()
-        .add_service(BrongnalServer::new(service))
-        .serve(server_addr.into())
+        .add_service(BrongnalServer::new(MemoryServer::default()))
+        .add_service(reflection_service)
+        .serve(server_addr)
         .await?;
 
     Ok(())
