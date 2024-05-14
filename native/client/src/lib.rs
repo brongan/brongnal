8963611f275464fdd5e77085ff52c0be0f8d4e94
Brennan Tracy <brennantracy4@gmail.com>
2024-05-13 17:02:43 -0700

    Isolate TUI client model from view.

--- native/client/src/lib.rs
+++ native/client/src/lib.rs
@@ -3,9 +3,15 @@ use chacha20poly1305::aead::OsRng;
 use chacha20poly1305::{ChaCha20Poly1305, KeyInit};
 use ed25519_dalek::SigningKey;
 use protocol::bundle::{create_prekey_bundle, sign_bundle};
-use protocol::x3dh::{x3dh_initiate_recv, x3dh_initiate_send, SignedPreKey, SignedPreKeys};
+use protocol::x3dh::{
+    x3dh_initiate_recv, x3dh_initiate_send, Message, SignedPreKey, SignedPreKeys,
+};
+use server::proto::brongnal_client::BrongnalClient;
+use server::proto::{
+    RegisterPreKeyBundleRequest, RequestPreKeysRequest, RetrieveMessagesRequest, SendMessageRequest,
+};
 use std::collections::HashMap;
-use std::io::{stdin, BufRead, BufReader};
+use tonic::transport::Channel;
 use x25519_dalek::{PublicKey as X25519PublicKey, StaticSecret as X25519StaticSecret};
 
 pub trait X3DHClient {
@@ -101,5 +107,99 @@ impl X3DHClient for MemoryClient {
             pre_keys,
             signature: otks.signature,
         }
+    }
+}
+
+pub struct BrongnalUser {
+    stub: BrongnalClient<Channel>,
+    x3dh_client: Box<dyn X3DHClient>,
+    name: Option<String>,
+}
+
+impl BrongnalUser {
+    pub async fn memory_user() -> Result<Self> {
+        Ok(BrongnalUser {
+            stub: BrongnalClient::connect("https://signal.brongan.com:443").await?,
+            x3dh_client: Box::new(MemoryClient::new()),
+            name: None,
+        })
+    }
+
+    pub async fn register(&mut self, name: &str) -> Result<()> {
+        self.name = Some(name.to_owned());
+        println!("Registering {name}!");
+        let ik = self
+            .x3dh_client
+            .get_identity_key()?
+            .verifying_key()
+            .as_bytes()
+            .to_vec();
+        let spk = self.x3dh_client.get_spk()?;
+        let otk_bundle = self.x3dh_client.add_one_time_keys(100);
+        let request = tonic::Request::new(RegisterPreKeyBundleRequest {
+            ik: Some(ik),
+            identity: self.name.clone(),
+            spk: Some(spk.into()),
+            otk_bundle: Some(otk_bundle.into()),
+        });
+        self.stub.register_pre_key_bundle(request).await?;
+        println!("Registered: {name}!");
+        Ok(())
+    }
+
+    pub async fn message(&mut self, name: &str, message: &str) -> Result<()> {
+        let message = message.as_bytes();
+        println!("Messaging {name}.");
+        let request = tonic::Request::new(RequestPreKeysRequest {
+            identity: Some(name.to_owned()),
+        });
+        let response = self.stub.request_pre_keys(request).await?;
+        let (_sk, message) = x3dh_initiate_send(
+            response.into_inner().try_into()?,
+            &self.x3dh_client.get_identity_key()?,
+            message,
+        )?;
+        let request = tonic::Request::new(SendMessageRequest {
+            recipient_identity: Some(name.to_owned()),
+            message: Some(message.into()),
+        });
+        self.stub.send_message(request).await?;
+        println!("Message Sent!");
+        Ok(())
+    }
+
+    pub async fn get_messages(&mut self) -> Result<()> {
+        let response = self
+            .stub
+            .retrieve_messages(RetrieveMessagesRequest {
+                identity: self.name.clone(),
+            })
+            .await?;
+        let messages = response.into_inner().messages;
+        println!("Retrieved {} messages.", messages.len());
+        for message in messages {
+            let Message {
+                sender_identity_key,
+                ephemeral_key,
+                otk,
+                ciphertext,
+            } = message.try_into()?;
+            let otk = if let Some(otk) = otk {
+                Some(self.x3dh_client.fetch_wipe_one_time_secret_key(&otk)?)
+            } else {
+                None
+            };
+            let (_sk, message) = x3dh_initiate_recv(
+                &self.x3dh_client.get_identity_key()?,
+                &self.x3dh_client.get_pre_key()?,
+                &sender_identity_key,
+                ephemeral_key,
+                otk,
+                &ciphertext,
+            )?;
+            let message = String::from_utf8(message)?;
+            println!("{message}");
+        }
+        Ok(())
     }
 }
--- native/client/src/main.rs
+++ native/client/src/main.rs
@@ -1,16 +1,11 @@
-use ::client::{MemoryClient, X3DHClient};
 use anyhow::Result;
+use client::BrongnalUser;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{alphanumeric1, multispace1};
 use nom::combinator::map;
 use nom::sequence::preceded;
 use nom::IResult;
-use protocol::x3dh::{x3dh_initiate_recv, x3dh_initiate_send, Message};
-use server::proto::brongnal_client::BrongnalClient;
-use server::proto::{
-    RegisterPreKeyBundleRequest, RequestPreKeysRequest, RetrieveMessagesRequest, SendMessageRequest,
-};
 use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Debug)]
@@ -51,8 +46,7 @@ fn parse_command(input: &str) -> IResult<&str, Command> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut stub = BrongnalClient::connect("https://signal.brongan.com:443").await?;
-    let mut x3dh_client = MemoryClient::new();
+    let mut user = BrongnalUser::memory_user().await?;
 
     println!(
         r#"Commands:
@@ -62,110 +56,15 @@ async fn main() -> Result<()> {
         Type Control-D (on Unix) or Control-Z (on Windows)
         to close the connection."#
     );
-    let mut my_name = "".to_string();
 
     let mut command_lines = BufReader::new(stdin()).lines();
     while let Some(command) = command_lines.next() {
         let command = command?;
         let (_, command) = parse_command(&command).map_err(|e| e.to_owned())?;
         let result: Result<()> = match &command {
-            Command::Register(name) => {
-                my_name = name.clone();
-                {
-                    let identity = my_name.clone();
-                    let client = &mut x3dh_client;
-                    let stub = &mut stub;
-                    async move {
-                        println!("Registering {identity}!");
-                        let ik = client
-                            .get_identity_key()?
-                            .verifying_key()
-                            .as_bytes()
-                            .to_vec();
-                        let spk = client.get_spk()?;
-                        let otk_bundle = client.add_one_time_keys(100);
-                        let request = tonic::Request::new(RegisterPreKeyBundleRequest {
-                            ik: Some(ik),
-                            identity: Some(identity.clone()),
-                            spk: Some(spk.into()),
-                            otk_bundle: Some(otk_bundle.into()),
-                        });
-                        stub.register_pre_key_bundle(request).await?;
-                        println!("Registered: {identity}!");
-                        Ok(())
-                    }
-                }
-                .await
-            }
-            Command::Message(name, message) => {
-                {
-                    let recipient_identity = name.clone();
-                    let client = &x3dh_client;
-                    let stub = &mut stub;
-                    let message = message.as_bytes();
-                    async move {
-                        println!("Messaging {recipient_identity}.");
-                        let request = tonic::Request::new(RequestPreKeysRequest {
-                            identity: Some(recipient_identity.clone()),
-                        });
-                        let response = stub.request_pre_keys(request).await?;
-                        let (_sk, message) = x3dh_initiate_send(
-                            response.into_inner().try_into()?,
-                            &client.get_identity_key()?,
-                            message,
-                        )?;
-                        let request = tonic::Request::new(SendMessageRequest {
-                            recipient_identity: Some(recipient_identity),
-                            message: Some(message.into()),
-                        });
-                        stub.send_message(request).await?;
-                        println!("Message Sent!");
-                        Ok(())
-                    }
-                }
-                .await
-            }
-            Command::GetMessages => {
-                {
-                    let name = my_name.clone();
-                    let client = &mut x3dh_client;
-                    let stub = &mut stub;
-                    async move {
-                        let response = stub
-                            .retrieve_messages(RetrieveMessagesRequest {
-                                identity: Some(name.clone()),
-                            })
-                            .await?;
-                        let messages = response.into_inner().messages;
-                        println!("Retrieved {} messages.", messages.len());
-                        for message in messages {
-                            let Message {
-                                sender_identity_key,
-                                ephemeral_key,
-                                otk,
-                                ciphertext,
-                            } = message.try_into()?;
-                            let otk = if let Some(otk) = otk {
-                                Some(client.fetch_wipe_one_time_secret_key(&otk)?)
-                            } else {
-                                None
-                            };
-                            let (_sk, message) = x3dh_initiate_recv(
-                                &client.get_identity_key()?,
-                                &client.get_pre_key()?,
-                                &sender_identity_key,
-                                ephemeral_key,
-                                otk,
-                                &ciphertext,
-                            )?;
-                            let message = String::from_utf8(message)?;
-                            println!("{message}");
-                        }
-                        Ok(())
-                    }
-                }
-                .await
-            }
+            Command::Register(name) => user.register(name).await,
+            Command::Message(name, message) => user.message(name, message).await,
+            Command::GetMessages => user.get_messages().await,
         };
         if let Err(e) = result {
             eprintln!("Command {command:?} failed: {e}");
