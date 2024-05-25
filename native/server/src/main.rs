a900e9ca0b177da5594c7e0b654f551781dcec44
Brennan Tracy <brennantracy4@gmail.com>
2024-05-24 22:34:48 -0700

    Add Gossamer rpc service.

--- native/server/src/main.rs
+++ native/server/src/main.rs
@@ -1,8 +1,13 @@
+use server::proto::gossamer_server::GossamerServer;
 use server::proto::{brongnal_server::BrongnalServer, FILE_DESCRIPTOR_SET};
 use server::MemoryServer;
 use std::net::{IpAddr, Ipv4Addr};
 use tonic::transport::Server;
 use tonic_reflection::server::Builder;
+
+use crate::gossamer::InMemoryGossamer;
+
+mod gossamer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,6 +21,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Server::builder()
         .add_service(BrongnalServer::new(MemoryServer::default()))
+        .add_service(GossamerServer::new(InMemoryGossamer {}))
         .add_service(reflection_service)
         .serve(server_addr)
         .await?;
