7553433de6f1b9ff1c91aa8c93c50536ace57bf8
Brennan Tracy <brennantracy4@gmail.com>
2024-10-15 18:08:17 -0700

    Fix Android Build
    * `nix flake update`
    * `cargo update`
    * Updated `rinf` version and changed dependent code.
    * added `android:enableOnBackInvokedCallback="true"` to stop annoying
      nags on Android debug log
    * Flutter passes database directory to Rust. Checks XDG first and uses
      Flutter on non-Linux platforms.
    * Registration is handled separately on first time vs non-first time
      startup.
    * Added sqlite3 explicit dependency in flutter.

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -1,6 +1,5 @@
-use crate::messages::brongnal::{RegisterUserResponse, SendMessage};
+use crate::messages::*;
 use client::{listen, message, register, sqlite_client::SqliteClient, DecryptedMessage};
-use messages::brongnal::{ReceivedMessage, RegisterUserRequest};
 use proto::service::brongnal_client::BrongnalClient;
 use rinf::debug_print;
 use std::path::PathBuf;
@@ -14,30 +13,43 @@ mod messages;
 
 rinf::write_interface!();
 
-async fn handle_register_user(
+async fn register_and_listen(
     mut stub: BrongnalClient<Channel>,
     client: Arc<Mutex<SqliteClient>>,
     tx: Sender<DecryptedMessage>,
+    username: String,
 ) {
-    let mut receiver = RegisterUserRequest::get_dart_signal_receiver().unwrap();
+    match register(&mut stub, client.clone(), username.clone()).await {
+        Ok(_) => {
+            debug_print!("Registered {username}");
+        }
+        Err(e) => {
+            debug_print!("Failed to register {username} with error: {e}");
+            return;
+        }
+    }
+    if let Err(e) = listen(stub, client, username, tx).await {
+        debug_print!("Listen terminated with: {e}");
+    }
+}
+
+async fn handle_register_user(
+    stub: BrongnalClient<Channel>,
+    client: Arc<Mutex<SqliteClient>>,
+    tx: Sender<DecryptedMessage>,
+) {
+    let receiver = RegisterUserRequest::get_dart_signal_receiver();
     while let Some(dart_signal) = receiver.recv().await {
         let message: RegisterUserRequest = dart_signal.message;
         match message.username {
             Some(name) => {
                 debug_print!("Received request to register {name}");
-                match register(&mut stub, client.clone(), name.clone()).await {
-                    Ok(_) => {
-                        debug_print!("Registered {name}");
-                    }
-                    Err(e) => {
-                        debug_print!("Failed to register {name} with error: {e}");
-                    }
-                }
-                let client = client.clone();
-                let stub = stub.clone();
-                let listen_name = name.clone();
-                let tx = tx.clone();
-                tokio::spawn(listen(stub, client, listen_name, tx));
+                tokio::spawn(register_and_listen(
+                    stub.clone(),
+                    client.clone(),
+                    tx.clone(),
+                    name.clone(),
+                ));
                 RegisterUserResponse {
                     username: Some(name),
                 }
@@ -51,7 +63,7 @@ async fn handle_register_user(
 }
 
 async fn handle_send_message(mut stub: BrongnalClient<Channel>, client: Arc<Mutex<SqliteClient>>) {
-    let mut receiver = SendMessage::get_dart_signal_receiver().unwrap();
+    let receiver = SendMessage::get_dart_signal_receiver();
     while let Some(dart_signal) = receiver.recv().await {
         let req: SendMessage = dart_signal.message;
         match message(
@@ -71,19 +83,36 @@ async fn handle_send_message(mut stub: BrongnalClient<Channel>, client: Arc<Mute
     }
 }
 
+#[tokio::main]
 async fn main() {
     let stub = BrongnalClient::connect("https://signal.brongan.com:443")
         .await
         .unwrap();
 
-    let identity_key_path = PathBuf::from("identity_key");
-    let db_path = PathBuf::from("keys.sqlite");
+    let receiver = RustStartup::get_dart_signal_receiver();
+    let (db_dir, username) = loop {
+        if let Some(dart_signal) = receiver.recv().await {
+            let message = dart_signal.message;
+            break (
+                PathBuf::from(message.database_directory().to_owned()),
+                message.username,
+            );
+        }
+    };
+
+    debug_print!("Database Directory: {db_dir:?}");
+    let identity_key_path = db_dir.join("identity_key");
+    let db_path = db_dir.join("keys.sqlite");
     let client = Arc::new(Mutex::new(
         SqliteClient::new(&identity_key_path, &db_path).unwrap(),
     ));
 
     let (tx, mut rx) = mpsc::channel(100);
-    tokio::spawn(handle_register_user(stub.clone(), client.clone(), tx));
+    if let Some(username) = username {
+        register_and_listen(stub.clone(), client.clone(), tx, username).await;
+    } else {
+        tokio::spawn(handle_register_user(stub.clone(), client.clone(), tx));
+    }
     tokio::spawn(handle_send_message(stub.clone(), client.clone()));
 
     while let Some(decrypted) = rx.recv().await {
@@ -100,4 +129,5 @@ async fn main() {
         }
         .send_signal_to_dart();
     }
+    rinf::dart_shutdown().await;
 }
