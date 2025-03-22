1481fc9a14e940915bcfc43aed4d42846506b1ea
Brennan Tracy <brennantracy4@gmail.com>
2025-03-21 23:47:47 -0700

    Move gRPC protos to versioned directory.

--- native/proto/build.rs
+++ native/proto/build.rs
@@ -4,7 +4,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("service_descriptor.bin"))
-        .compile(&["service.proto", "gossamer.proto"], &["proto"])
+        .compile(
+            &["service/v1/service.proto", "gossamer/v1/gossamer.proto"],
+            &["proto"],
+        )
         .unwrap();
     Ok(())
 }
