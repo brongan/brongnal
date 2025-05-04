11a9ca361d986942cc8fec599b643145c0990a78
Brennan Tracy <brennantracy4@gmail.com>
2025-05-03 18:12:39 -0700

    Working on moving the state to rust.

--- native/client/src/client.rs
+++ native/client/src/client.rs
@@ -1,13 +1,25 @@
 use crate::{ClientError, ClientResult};
+use base64::{engine::general_purpose::STANDARD as base64, Engine as _};
 use chacha20poly1305::aead::OsRng;
 use ed25519_dalek::{SigningKey, VerifyingKey};
+use proto::ApplicationMessage;
 use protocol::bundle::{create_prekey_bundle, sign_bundle};
 use protocol::x3dh;
+use rinf::{RustSignal, SignalPiece};
 use rusqlite::{params, Connection};
-use std::time::{SystemTime, UNIX_EPOCH};
+use serde::Serialize;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use strum_macros::FromRepr;
 use tracing::info;
 use x25519_dalek::{PublicKey as X25519PublicKey, StaticSecret as X25519StaticSecret};
 use x3dh::{SignedPreKey, SignedPreKeys};
+
+fn time_now() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+}
 
 #[derive(Clone, Copy, strum_macros::Display)]
 #[repr(u32)]
@@ -22,20 +34,39 @@ pub struct X3DHClient {
     ik: SigningKey,
 }
 
-fn create_key_table(connection: &Connection) -> rusqlite::Result<()> {
+fn create_tables(connection: &Connection) -> rusqlite::Result<()> {
     connection.pragma_update(None, "journal_mode", "WAL")?;
     connection.pragma_update(None, "synchronous", "normal")?;
     connection.pragma_update(None, "foreign_keys", "on")?;
 
-    connection.execute(
-        "CREATE TABLE IF NOT EXISTS keys (
-             public_key BLOB PRIMARY KEY,
-             private_key BLOB NOT NULL,
-             key_type INTEGER NOT NULL,
-             creation_time INTEGER NOT NULL
-         )",
-        (),
+    connection.execute_batch(
+        "
+                    BEGIN;
+                    CREATE TABLE IF NOT EXISTS keys (
+                        public_key BLOB PRIMARY KEY,
+                        private_key BLOB NOT NULL,
+                        key_type INTEGER NOT NULL,
+                        creation_time INTEGER NOT NULL
+                    );
+                    CREATE TABLE IF NOT EXISTS users (
+                        username TEXT PRIMARY KEY,
+                        created_at INTEGER NOT NULL,
+                        name TEXT,
+                        profile_pic BLOB
+                    );
+                    CREATE TABLE IF NOT EXISTS messages (
+                        message_id INTEGER PRIMARY KEY,
+                        sender TEXT NOT NULL,
+                        receiver TEXT NOT NULL,
+                        creation_time INTEGER NOT NULL,
+                        state INTEGER NOT NULL,
+                        text TEXT,
+                        FOREIGN KEY(sender) REFERENCES users(username),
+                        FOREIGN KEY(receiver) REFERENCES users(username)
+                    );
+                    COMMIT;",
     )?;
+
     Ok(())
 }
 
@@ -131,11 +162,124 @@ fn opk_count(connection: &Connection) -> rusqlite::Result<u32> {
     )
 }
 
+#[derive(Clone, Copy, strum_macros::Display, Serialize, FromRepr)]
+#[repr(u8)]
+pub enum MessageState {
+    Sending,
+    Sent,
+    Delivered,
+}
+
+#[derive(Serialize, RustSignal)]
+pub struct MessagesModel {
+    pub messages: Vec<MessageModel>,
+}
+
+#[derive(Serialize, RustSignal, SignalPiece)]
+pub struct MessageModel {
+    sender: String,
+    receiver: String,
+    creation_time: SystemTime,
+    state: MessageState,
+    text: String,
+}
+
+impl std::fmt::Display for MessageModel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let &MessageModel {
+            ref sender,
+            ref receiver,
+            creation_time,
+            state,
+            ref text,
+        } = self;
+        write!(
+            f,
+            "From: {sender} To: {receiver} at {creation_time:#?} with {state}\n{text}"
+        )?;
+        Ok(())
+    }
+}
+
+type MessageId = i64;
+
+pub fn add_user(
+    connection: &Connection,
+    username: &str,
+    name: Option<String>,
+    profile_pic: Vec<u8>,
+) -> rusqlite::Result<()> {
+    connection.execute(
+        "INSERT OR IGNORE INTO users (username, created_at, name, profile_pic) VALUES ($1, $2)",
+        params![username, time_now(), name, profile_pic],
+    )?;
+    Ok(())
+}
+
+pub fn set_received(
+    connection: &Connection,
+    our_username: &str,
+    message: ApplicationMessage,
+) -> rusqlite::Result<()> {
+    connection.execute("INSERT OR IGNORE INTO messages (sender, receiver, creation_time, state, text) VALUES ($1, $2, $3, $4, $5)", 
+        params![
+            message.sender, our_username, time_now(), MessageState::Delivered as u8, message.text
+        ]
+    )?;
+    Ok(())
+}
+
+fn persist_state(
+    connection: &Connection,
+    sender: &str,
+    receiver: &str,
+    message: &str,
+    state: MessageState,
+) -> rusqlite::Result<MessageId> {
+    Ok(connection.query_row("INSERT OR IGNORE INTO messages (sender, receiver, creation_time, state, text) VALUES ($1, $2, $3, $4, $5) RETURNING rowid", 
+        params![
+            sender, receiver, time_now(), state as u8, message
+        ],
+        |row| row.get(0),
+    )?)
+}
+
+fn update_state(
+    connection: &Connection,
+    id: MessageId,
+    state: MessageState,
+) -> rusqlite::Result<()> {
+    // Returns the first row updated so that a missing message results in an error.
+    let _: u8 = connection.query_row(
+        "UPDATE messages SET state = ?2 WHERE id = ?1 RETURNING state",
+        params![id, state as u8],
+        |row| row.get(0),
+    )?;
+    Ok(())
+}
+
+fn get_conversations(connection: &Connection) -> rusqlite::Result<MessagesModel> {
+    let mut stmt =
+        connection.prepare("SELECT sender, receiver, creation_time, state, text FROM messages")?;
+    let mut message_iter = stmt.query_map([], |row| {
+        Ok(MessageModel {
+            sender: row.get(0)?,
+            receiver: row.get(1)?,
+            creation_time: SystemTime::UNIX_EPOCH + Duration::from_secs(row.get(2)?),
+            state: MessageState::from_repr(row.get(3)?).unwrap(),
+            text: row.get(4)?,
+        })
+    })?;
+    Ok(MessagesModel {
+        messages: message_iter.try_collect()?,
+    })
+}
+
 impl X3DHClient {
     pub async fn new(connection: tokio_rusqlite::Connection) -> ClientResult<X3DHClient> {
         let ik = connection
             .call(|connection| {
-                create_key_table(connection)?;
+                create_tables(connection)?;
                 lazy_init_pre_key(connection)?;
                 Ok(lazy_init_identity_key(connection)?)
             })
@@ -145,9 +289,7 @@ impl X3DHClient {
         let sqlite_client = X3DHClient { connection, ik };
         Ok(sqlite_client)
     }
-}
 
-impl X3DHClient {
     pub async fn fetch_wipe_opk(
         &self,
         one_time_prekey: X25519PublicKey,
@@ -190,8 +332,7 @@ impl X3DHClient {
         self.connection
             .call(move |connection| {
                 let pre_key = load_pre_key(connection)?.unwrap();
-                #[allow(deprecated)]
-                let pubkey = base64::encode(pre_key.to_bytes());
+                let pubkey = base64.encode(pre_key.to_bytes());
                 info!("Signing pre key: {pubkey}");
                 Ok(SignedPreKey {
                     pre_key: X25519PublicKey::from(&pre_key),
@@ -228,6 +369,41 @@ impl X3DHClient {
             signature: opks.signature,
         })
     }
+
+    pub async fn persist_message(
+        &self,
+        sender: String,
+        receiver: String,
+        message: String,
+        state: MessageState,
+    ) -> ClientResult<MessageId> {
+        self.connection
+            .call(move |connection| {
+                Ok(persist_state(
+                    connection, &sender, &receiver, &message, state,
+                )?)
+            })
+            .await
+            .map_err(ClientError::TokioSqlite)
+    }
+
+    pub async fn persist_message_state(
+        &self,
+        message_id: MessageId,
+        state: MessageState,
+    ) -> ClientResult<()> {
+        self.connection
+            .call(move |connection| Ok(update_state(connection, message_id, state)?))
+            .await
+            .map_err(ClientError::TokioSqlite)
+    }
+
+    pub async fn get_messages(&self) -> ClientResult<MessagesModel> {
+        self.connection
+            .call(move |connection| Ok(get_conversations(connection)?))
+            .await
+            .map_err(ClientError::TokioSqlite)
+    }
 }
 
 #[cfg(test)]
@@ -240,7 +416,7 @@ mod tests {
     #[test]
     fn load_identity_key_not_found() -> Result<()> {
         let connection = Connection::open_in_memory()?;
-        create_key_table(&connection)?;
+        create_tables(&connection)?;
         assert_eq!(load_identity_key(&connection)?, None);
         Ok(())
     }
@@ -248,7 +424,7 @@ mod tests {
     #[test]
     fn load_pre_key_not_found() -> Result<()> {
         let connection = Connection::open_in_memory()?;
-        create_key_table(&connection)?;
+        create_tables(&connection)?;
         assert!(load_pre_key(&connection)?.is_none());
         Ok(())
     }
@@ -256,7 +432,7 @@ mod tests {
     #[test]
     fn init_identity_key() -> Result<()> {
         let connection = Connection::open_in_memory()?;
-        create_key_table(&connection)?;
+        create_tables(&connection)?;
         lazy_init_identity_key(&connection)?;
 
         let key = load_identity_key(&connection)?;
@@ -270,7 +446,7 @@ mod tests {
     #[test]
     fn init_pre_key() -> Result<()> {
         let connection = Connection::open_in_memory()?;
-        create_key_table(&connection)?;
+        create_tables(&connection)?;
         lazy_init_pre_key(&connection)?;
 
         let key = load_pre_key(&connection)?;
